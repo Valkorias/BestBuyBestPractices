aa767b1efb6ca0a87f0bb92c1e16b1aed6914424
David Figone <david.louis.figone@gmail.com>
2022-11-16 12:29:00 -0600

    Final Commit

--- BestBuyBestPractices/Program.cs
+++ BestBuyBestPractices/Program.cs
@@ -12,17 +12,34 @@ string connString = config.GetConnectionString("DefaultConnection");
 
 IDbConnection conn = new MySqlConnection(connString);
 
-var departmentRepo = new DapperDepartmentRepository(conn);
+#region Department Section
+//var departmentRepo = new DapperDepartmentRepository(conn);
 
-departmentRepo.InsertDepartment("John's New Department");
+//departmentRepo.InsertDepartment("John's New Department");
 
-var departments = departmentRepo.GetAllDepartments();
+//var departments = departmentRepo.GetAllDepartments();
 
-foreach (var department in departments)
+//foreach (var department in departments)
+//{
+//    Console.WriteLine(department.DepartmentID);
+//    Console.WriteLine(department.Name);
+//    Console.WriteLine();
+//    Console.WriteLine();
+//}
+#endregion
+
+#region Products Section
+var productRepository = new DapperProductRepository(conn);
+var products = productRepository.GetAllProducts();
+foreach (var product in products)
 {
-    Console.WriteLine(department.DepartmentID);
-    Console.WriteLine(department.Name);
+    Console.WriteLine(product.ProductID);
+    Console.WriteLine(product.Name);
+    Console.WriteLine(product.Price);
+    Console.WriteLine(product.CategoryID);
+    Console.WriteLine(product.OnSale);
+    Console.WriteLine(product.StockLevel);
     Console.WriteLine();
     Console.WriteLine();
 }
-
+#endregion
